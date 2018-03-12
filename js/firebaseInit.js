be52d3c8430702fcc0992eb2d325bf9a4fb0e267
DESKTOP-KDNJ46P\Karthick <karthick.veeramani@gmail.com>
2018-03-13 01:24:18 +0530

    Signup, username password created in firebase

---
+++ js/firebaseInit.js
@@ -0,0 +1,11 @@
+
+   /* Initialize Firebase */
+    var config = {
+      apiKey: "AIzaSyBeiuEndX8d5Y_2vNX-SLwd3xMDBfv193Y",
+      authDomain: "test-e80c6.firebaseapp.com",
+      databaseURL: "https://test-e80c6.firebaseio.com",
+      projectId: "test-e80c6",
+      storageBucket: "test-e80c6.appspot.com",
+      messagingSenderId: "533571532202"
+    };
+    firebase.initializeApp(config);
